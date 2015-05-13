f8f8959cdbfcf6ce3254b6eda386d6de2a6b2362
Milam <charles.milam@gmail.com>
2015-05-13 18:32:02 -0500

    Modify version of jquery being used.

--- goodmorningtree/header.php
+++ goodmorningtree/header.php
@@ -199,7 +199,7 @@
             </div>
     <?php	endif; ?>
 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
     <script type="text/javascript">
         $(document).ready(function(){
