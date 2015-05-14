596a9fd7456f3c604d761dfb23f2335d70d926fc
Milam <charles.milam@gmail.com>
2015-05-14 15:02:01 -0500

    Remove un-needed jQuery validate call.

--- goodmorningtree/scripts/script.js
+++ goodmorningtree/scripts/script.js
@@ -13,18 +13,18 @@ jQuery(document).ready(function($){
 });
 
 // initialise Superfish Menu
-jQuery(document).ready(function($){
-    $("ul.sf-menu").supersubs({
-	minWidth:    12,   // minimum width of sub-menus in em units
-	maxWidth:    32,   // maximum width of sub-menus in em units
-	extraWidth:  1     // extra width can ensure lines don't sometimes turn over
-			   // due to slight rounding differences and font-family
-    }).superfish({	   // call supersubs first, then superfish, so that subs are not display:none when measuring. Call before initialising containing tabs for same reason.
-	delay:       500,  // the delay in milliseconds that the mouse can remain outside a submenu without it closing
-	autoArrows:  false,
-	dropShadows: false
-    });
-});
+// jQuery(document).ready(function($){
+//     $("ul.sf-menu").supersubs({
+// 	minWidth:    12,   // minimum width of sub-menus in em units
+// 	maxWidth:    32,   // maximum width of sub-menus in em units
+// 	extraWidth:  1     // extra width can ensure lines don't sometimes turn over
+// 			   // due to slight rounding differences and font-family
+//     }).superfish({	   // call supersubs first, then superfish, so that subs are not display:none when measuring. Call before initialising containing tabs for same reason.
+// 	delay:       500,  // the delay in milliseconds that the mouse can remain outside a submenu without it closing
+// 	autoArrows:  false,
+// 	dropShadows: false
+//     });
+// });
 
 // ThumbCaption script
 jQuery(document).ready(function($){
@@ -44,34 +44,6 @@ jQuery(document).ready(function($){
     function(){
 	    var info=$(this).find(".hover-opacity");
 	    info.stop().animate({opacity:1},400);
-    });
-});
-
-
-
-// jQuery Validate
-jQuery(document).ready(function($){
-
-    $("#contactForm").validate({
-	rules: {
-		contact_name: {
-			required: true,
-			minlength: 2
-		},
-		contact_email: {
-			required: true,
-			email: true
-		},
-		contact_message: "required"
-	},
-	messages: {
-		contact_name: {
-			required: "Please enter a name",
-			minlength: "Your name must consist of at least 2 characters"
-		},
-		contact_email: "Please enter a valid email address",
-		contact_message: "<br />Please enter your message"
-	}
     });
 });
 
